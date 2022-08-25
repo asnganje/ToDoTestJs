66395fa1b067646c96d8f73e6ea5d40906741042
asnganje <abd.nganje@gmail.com>
2022-08-25 19:23:20 +0300

    fix tests

--- src/taskService.test.js
+++ src/taskService.test.js
@@ -1,3 +1,58 @@
+const storeTask = require('./taskService.js');
+// Unit-Test for SaveTask
+describe('saveTask()', () => {
+  it('saveTask must add task on existing list', () => {
+    // Arrange
+    const tasks = [
+      {
+        index: 1,
+        description: 'one',
+        isCompeleted: false,
+      },
+    ];
+    const newTask = {
+      index: 2,
+      description: 'two',
+      isCompeleted: true,
+    };
+    const expected = [
+      {
+        index: 1,
+        description: 'one',
+        isCompeleted: false,
+      },
+      {
+        index: 2,
+        description: 'two',
+        isCompeleted: true,
+      },
+    ];
+    // Act
+    const output = storeTask.saveTask(newTask, tasks);
+    // Assert
+    expect(output).toStrictEqual(expected);
+  });
+  it('saveTask must start from inde one', () => {
+    // Arrange
+    const tasks = [];
+    const newTask = {
+      index: 2,
+      description: 'two',
+      isCompeleted: true,
+    };
+    const expected = [
+      {
+        index: 1,
+        description: 'two',
+        isCompeleted: true,
+      },
+    ];
+    // Act
+    const output = storeTask.saveTask(newTask, tasks);
+    // Assert
+    expect(output).toStrictEqual(expected);
+  });
+});
 // Unit-Test for removeTask
 describe('removeTask()', () => {
   it('removeTask must remove task on existing list', () => {
