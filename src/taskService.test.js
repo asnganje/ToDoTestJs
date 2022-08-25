055ac19fdfe7422d0a45ced8e0488a757147d629
asnganje <abd.nganje@gmail.com>
2022-08-25 19:17:58 +0300

    update task tests

--- src/taskService.test.js
+++ src/taskService.test.js
@@ -1,21 +1,8 @@
-const storeTask = require('./taskService.js');
-// Unit-Test for SaveTask
-describe('saveTask()', () => {
-  it('saveTask must add task on existing list', () => {
+// Unit-Test for removeTask
+describe('removeTask()', () => {
+  it('removeTask must remove task on existing list', () => {
     // Arrange
     const tasks = [
-      {
-        index: 1,
-        description: 'one',
-        isCompeleted: false,
-      },
-    ];
-    const newTask = {
-      index: 2,
-      description: 'two',
-      isCompeleted: true,
-    };
-    const expected = [
       {
         index: 1,
         description: 'one',
@@ -27,18 +14,41 @@ describe('saveTask()', () => {
         isCompeleted: true,
       },
     ];
-    // Act
-    const output = storeTask.saveTask(newTask, tasks);
-    // Assert
-    expect(output).toStrictEqual(expected);
-  });
-  it('saveTask must start from index one', () => {
-    // Arrange
-    const tasks = [];
-    const newTask = {
+    const tobeRemoved = {
       index: 2,
       description: 'two',
       isCompeleted: true,
+    };
+    const expected = [
+      {
+        index: 1,
+        description: 'one',
+        isCompeleted: false,
+      },
+    ];
+    // Act
+    const output = storeTask.removeTask(tobeRemoved.index, tasks);
+    // Assert
+    expect(output).toStrictEqual(expected);
+  });
+  it('removeTask must remove task on existing list and fix index number', () => {
+    // Arrange
+    const tasks = [
+      {
+        index: 1,
+        description: 'one',
+        isCompeleted: false,
+      },
+      {
+        index: 2,
+        description: 'two',
+        isCompeleted: true,
+      },
+    ];
+    const tobeRemoved = {
+      index: 1,
+      description: 'one',
+      isCompeleted: false,
     };
     const expected = [
       {
@@ -48,7 +58,7 @@ describe('saveTask()', () => {
       },
     ];
     // Act
-    const output = storeTask.saveTask(newTask, tasks);
+    const output = storeTask.removeTask(tobeRemoved.index, tasks);
     // Assert
     expect(output).toStrictEqual(expected);
   });
